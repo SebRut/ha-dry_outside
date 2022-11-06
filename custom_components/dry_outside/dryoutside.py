c6ab584ee9eafd9a5aaedf2ae99a771ed6af7a56
Sebastian Rutofski <sebastian.rutofski@gmail.com>
2022-11-06 08:04:35 +0100

    add basic logic

--- custom_components/dry_outside/dryoutside.py
+++ custom_components/dry_outside/dryoutside.py
@@ -1,2 +1,8 @@
-async def should_dry_outside() -> bool:
-    return True
+async def should_dry_outside(temp_in_c: float, humidity: float) -> bool:
+    if temp_in_c <= 0:
+        return True
+
+    if humidity <= 40:
+        return True
+
+    return False
